69bddbdfad2914c96608ccecc703f7ca17f6f421
BorysArhat <argat3lt@gmail.com>
2022-08-10 22:50:37 +0300

    hw8 to check

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+import '../css/common.css';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const LOCAL_KEY = 'videoplayer-current-time';
+player.on('timeupdate', throttle(onPlay, 1000));
+function onPlay({ seconds }) {
+  localStorage.setItem(LOCAL_KEY, seconds);
+}
+setCurrentTime();
+function setCurrentTime() {
+  const playedTime = localStorage.getItem(LOCAL_KEY);
+  if (playedTime) {
+    player.setCurrentTime(playedTime);
+  }
+}
 
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
-
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(onInput, 500));
+const formData = localStorage.getItem('feedback-form-state')
+  ? JSON.parse(localStorage.getItem('feedback-form-state'))
+  : {};
+form.addEventListener('submit', onSubmit);
+function onSubmit(e) {
+  e.preventDefault();
+  const dataParsed = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const email = e.currentTarget.elements.email.value;
+  const message = e.currentTarget.elements.message.value;
+  if (email === '' || message === '') {
+    return alert('Ви забули щось написати 😤');
+  }
+  console.log(dataParsed);
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+function onInput({ target }) {
+  formData[target.name] = target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+function setFormValues() {
+  const grabedInfo = localStorage.getItem('feedback-form-state');
+  console.log(grabedInfo);
+  if (grabedInfo) {
+    const data = JSON.parse(grabedInfo);
+    if (data.email) {
+      form.elements.email.value = data.email;
+    }
+    if (data.message) {
+      form.elements.message.value = data.message;
+    }
+  }
+}
+setFormValues();
